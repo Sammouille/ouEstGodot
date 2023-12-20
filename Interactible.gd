5685103c9eea5e14f9684dcfc09acfdf097f10d4
ffdecaesteker <118174383+EDK-pro@users.noreply.github.com>
2023-12-20 14:17:42 +0100

    En train de rajouter le fonctionnement des objets

--- Interactible.gd
+++ Interactible.gd
@@ -1,15 +1,24 @@
 extends Node2D
-class_name Objets 
 
-var nom: String
-var description: String
+var pickable:bool = false
+
+signal clicked_on_object
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	#texture()
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	pass
+	if pickable:
+		if Input.is_action_just_pressed("ui_click"):
+			clicked_on_object.emit()
+
+
+func _on_mouse_entered():
+	pickable = true
+
+func _on_mouse_exited():
+	pickable = false
--- main.gd
+++ main.gd
@@ -1,18 +1,23 @@
 extends Node2D
+
+
 var file
 var content
-
+var objet = Objets.new()
+@export var sprite: AnimatedSprite2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	file = FileAccess.open("res://Dialogue.txt", FileAccess.READ)
 	content = file.get_as_text()
 	print(content.split("\n"))
-	pass # Replace with function body.
-
-
+	$AnimatedSprite2D.frame = 1
+		
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
+	
 	if Input.is_action_just_pressed("ui_click"):
+		$AnimatedSprite2D.frame = 0
+
 		$TextBox.visible = false
 
 
