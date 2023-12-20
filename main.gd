3dc65cff44a131679216d051d9e25f72d8205a00
sammouille_ <samuel.wolf@hotmail.fr>
2023-12-20 19:38:04 +0100

    ca va etre tererible

--- main.gd
+++ main.gd
@@ -5,6 +5,7 @@ var file
 var content
 var text_tab
 var objet = Objets.new()
+
 @export var sprite: AnimatedSprite2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -51,7 +52,7 @@ func _on_node_2d_clicked_on_object():
 
 
 func _on_pnj_1_clicked_on_object():
-	SignalBus.entrée_journal.emit()
+	pass
 
 
 func _on_mask_smile_clicked_on_object():
@@ -60,7 +61,8 @@ func _on_mask_smile_clicked_on_object():
 	$TextBox/Panel/Description.text = text_tab[1]
 	
 	$AnimatedSprite2D.frame = 11
-	SignalBus.entrée_journal.emit("masque_sourire")
+	
+
 
 
 func _on_mask_sad_clicked_on_object():
@@ -69,6 +71,8 @@ func _on_mask_sad_clicked_on_object():
 	$TextBox/Panel/Description.text = text_tab[1]
 	
 	$AnimatedSprite2D.frame = 12
+	
+
 
 func _on_skull_clicked_on_object():
 	make_sprite_and_textbox_visible()
@@ -78,12 +82,14 @@ func _on_skull_clicked_on_object():
 	$AnimatedSprite2D.frame = 0
 
 
+
 func _on_flower_clicked_on_object():
 	make_sprite_and_textbox_visible()
 	$TextBox/Panel/Nom.text = text_tab[14]
 	$TextBox/Panel/Description.text = text_tab[15]
 	
 	$AnimatedSprite2D.frame = 7
+
 
 func _on_onions_clicked_on_object():
 	make_sprite_and_textbox_visible()
@@ -92,6 +98,7 @@ func _on_onions_clicked_on_object():
 	
 	$AnimatedSprite2D.frame = 2
 
+
 func _on_glacoun_clicked_on_object():
 	make_sprite_and_textbox_visible()
 	$TextBox/Panel/Nom.text = text_tab[12]
@@ -99,12 +106,14 @@ func _on_glacoun_clicked_on_object():
 
 	$AnimatedSprite2D.frame = 6
 
+
 func _on_handcuff_clicked_on_object():
 	make_sprite_and_textbox_visible()
 	$TextBox/Panel/Nom.text = text_tab[18]
 	$TextBox/Panel/Description.text = text_tab[19]
 	
 	$AnimatedSprite2D.frame = 9
+
 
 
 func _on_hat_clicked_on_object():
@@ -115,6 +124,7 @@ func _on_hat_clicked_on_object():
 	$AnimatedSprite2D.frame = 4
 
 
+
 func _on_kirby_clicked_on_object():
 	make_sprite_and_textbox_visible()
 	$TextBox/Panel/Nom.text = text_tab[0]
@@ -122,12 +132,14 @@ func _on_kirby_clicked_on_object():
 	
 	$AnimatedSprite2D.frame = 10
 
+
 func _on_knife_clicked_on_object():
 	make_sprite_and_textbox_visible()
 	$TextBox/Panel/Nom.text = text_tab[6]
 	$TextBox/Panel/Description.text = text_tab[7]
 	
 	$AnimatedSprite2D.frame = 3
+
 
 
 func _on_painting_clicked_on_object():
@@ -138,12 +150,14 @@ func _on_painting_clicked_on_object():
 	$AnimatedSprite2D.frame = 15
 
 
+
 func _on_pocket_clicked_on_object():
 	make_sprite_and_textbox_visible()
 	$TextBox/Panel/Nom.text = text_tab[0]
 	$TextBox/Panel/Description.text = text_tab[1]
 	
 	$AnimatedSprite2D.frame = 13
+
 
 
 func _on_popcorn_clicked_on_object():
@@ -154,12 +168,14 @@ func _on_popcorn_clicked_on_object():
 	$AnimatedSprite2D.frame = 1
 
 
+
 func _on_ptitcham_clicked_on_object():
 	make_sprite_and_textbox_visible()
 	$TextBox/Panel/Nom.text = text_tab[16]
 	$TextBox/Panel/Description.text = text_tab[17]
 	
 	$AnimatedSprite2D.frame = 8
+
 
 
 func _on_stair_clicked_on_object():
@@ -178,9 +194,11 @@ func _on_thierry_clicked_on_object():
 	$AnimatedSprite2D.frame = 14
 
 
+
 func _on_jumelle_clicked_on_object():
 	make_sprite_and_textbox_visible()
 	$TextBox/Panel/Nom.text = text_tab[10]
 	$TextBox/Panel/Description.text = text_tab[11]
 	
 	$AnimatedSprite2D.frame = 5
+
--- signal_bus.gd
+++ signal_bus.gd
@@ -1,2 +1,22 @@
 extends Node
 signal entrée_journal(interaction:String)
+
+
+signal entrée_crane
+signal entrée_popcorn
+signal entrée_oignon
+signal entrée_couteau
+signal entrée_chapeau
+signal entrée_jumelles
+signal entrée_glacoun
+signal entrée_vin
+signal entrée_fleurs
+signal entrée_cham
+signal entrée_menottes
+signal entrée_joconde
+signal entrée_pnj1
+signal entrée_pnj2
+signal entrée_pnj3
+signal entrée_pnj4
+signal entrée_masque_rire
+signal entrée_masque_triste
