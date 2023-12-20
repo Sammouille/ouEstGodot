49d247171c4891b251e28f12e5c6d2a83cb3410e
sammouille_ <samuel.wolf@hotmail.fr>
2023-12-20 17:25:21 +0100

    merci pierre (moi dans 3 ans)

--- notes_journal/carnet.gd
+++ notes_journal/carnet.gd
@@ -1,11 +1,18 @@
-extends TextEdit
+extends Control
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	SignalBus.entrée_journal.connect(afficher)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func afficher():
+	print("pouet")
+
+	$TextureRect/VBoxContainer/entrée_de_journal2.show()
+
+#hide()
